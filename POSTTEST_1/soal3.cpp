fad9c3cf928a45f0b4f30fd86d5b9f1389c20a6e
Haekal <andihaekal338@gmail.com>
2026-03-01 20:39:26 +0800

    Soal 3

--- POSTTEST_1/soal3.cpp
+++ POSTTEST_1/soal3.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
 using namespace std;
 
+void balik(int* arr, int n){
+    int *awal = arr;
+    int *akhir = arr + (n - 1);
+
+    while(awal < akhir){
+        int wadah = *awal;
+        *awal = *akhir;
+        *akhir = wadah;
+        awal++;
+        akhir--;
+    }
+}
 int main(){
-    cout << "Test";
+    int total = 7;
+    int angka[total] = {2, 3, 5, 7, 11, 13, 17};
+    int angka1, angka2;
+    int *p1 = angka;
+    cout << "Array sebelum dibalik" << endl;
+    cout << "Data\t\t: {2, 3, 5, 7, 11, 13, 17}" << endl;
 
+    for(int i = 0; i < total; i++){
+        cout << "Nilai\t\t: " << *p1 << "\tAlamat\t\t: " << p1 << endl;
+        p1++;
+    }
+    cout << endl << endl;
 
+    balik(angka, total);
+    int *p2 = angka;
+    cout << "Array sesudah dibalik" << endl;
+    cout << "Data\t\t: {17, 13, 11, 7, 5, 3, 2}" << endl;
+
+    for(int i = 0; i < total; i++){
+        cout << "Nilai\t\t: " << *p2 << "\tAlamat\t\t: " << p2 << endl;
+        p2++;
+    }
+    
     return 0;
 }
