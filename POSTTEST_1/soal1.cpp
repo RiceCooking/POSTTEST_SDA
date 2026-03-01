1e4bc476cc66f7deb0f322afce69acd39f6d7003
Haekal <andihaekal338@gmail.com>
2026-03-01 10:57:38 +0800

    Soal 1 bagin Implementasikan fungsi FindMin di C++

--- POSTTEST_1/soal1.cpp
+++ POSTTEST_1/soal1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Test";
+    int IMO[8] = {1, 1, 2, 3, 5, 8, 13, 21};
+    int n = 8;
+    int nilaimin = IMO[0];
+    int indexmin = 0;
 
-
+    for(int i = 1;i < n;i++){
+        if(IMO[i] < nilaimin){
+            nilaimin = IMO[i];
+            indexmin = i;
+        }
+    }
+    cout << "Data\t\t\t: {1, 1, 2, 3, 5, 8, 13, 21" << '\n';
+    cout << "Nilai Minimum\t: " << nilaimin << endl;
+    cout << "Index\t\t\t: " << indexmin;
     return 0;
 }
