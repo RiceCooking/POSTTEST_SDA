5e708435818d572b3f7c75d814e3cba5e38e8943
Haekal <andihaekal338@gmail.com>
2026-03-01 13:06:32 +0800

    Soal 2

--- POSTTEST_1/soal2.cpp
+++ POSTTEST_1/soal2.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
+struct Mahasiswa
+{
+    string nama;
+    string NIM;
+    double IPK;
+};
+
 int main(){
-    cout << "Test";
+    int total  = 5;
+    Mahasiswa skom[total];
+    for(int i = 0; i < total; i++){
+        cout << "Data mahasiswa ke-" << i + 1 << endl;
+        cout << "Nama\t\t: ";getline(cin, skom[i].nama);
+        cout << "NIM\t\t: ";getline(cin, skom[i].NIM);
+        cout << "IPK\t\t: ";cin >> skom[i].IPK;
+        cout << endl;
+        cin.ignore();
+    }
 
+    int index = 0;
+    for(int i = 0; i < total; i++){
+        if(skom[i].IPK > skom[index].IPK){
+            index = i;
+        }
+    }
 
+    cout << endl << endl << "Mahasiswa dengan IPK tertinggi" << endl;
+    cout << "Nama\t\t: " << skom[index].nama << endl;
+    cout << "NIM\t\t: " << skom[index].NIM << endl;
+    cout << "IPK\t\t: " << skom[index].IPK << endl;
     return 0;
 }
